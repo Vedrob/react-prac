739164ed6b2be4b0a5f5cd598eab5516d0239f08
vedrob <vedanshsingh975@gmail.com>
2026-06-07 17:18:24 +0530

    props drilling usig array

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,29 @@ import './App.css'
 import Card from './components/card'
 
 function App() {
+  const arr = [
+    {
+      name : "Vedansh",
+      role : "Developer"
+    },
+    {
+      name : "Drax",
+      role : "Quality analyst"
+    },
+    {
+      name : "Sujal",
+      role : "Developer"
+    },
+    {
+      name : "KANE",
+      role : "Forward Deployed Engineer"
+    }];
   return (
+
       <div className="parent">
-        <Card user="Vedansh singh"/>
-        <Card user="Vansh Gupta"/>
-        <Card user="Shivansh"/>
-        <Card user="Chujay"/>
+        {arr.map(function(elem){
+          return <Card user ={elem.name} pos={elem.role}/>
+        })}
       </div>
   )
 }
