4eb859e0fe949a7d613dba1bacccc49294d3e982
vedrob <vedanshsingh975@gmail.com>
2026-06-07 14:14:34 +0530

    about props

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Card from './components/card'
+
+function App() {
+  return (
+      <div className="parent">
+        <Card user="Vedansh singh"/>
+        <Card user="Vansh Gupta"/>
+        <Card user="Shivansh"/>
+        <Card user="Chujay"/>
+      </div>
+  )
+}
+
+export default App
---
+++ src/components/card.jsx
@@ -0,0 +1,12 @@
+function Card(props) {
+    return (
+        <div className="card">
+            <img src="https://images.unsplash.com/photo-1780726624500-8de106966b6f?q=80&w=729&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <h1>{props.user}</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque soluta magnam aspernatur repellendus quo reprehenderit assumenda facilis asperiores molestiae aut.</p>
+            <button>view profile</button>
+        </div>
+    )
+}
+
+export default Card;
