f848a05d954f01559a536cc1e1cf7b99169fd1cc
vedrob <vedanshsingh975@gmail.com>
2026-06-09 20:18:20 +0530

    added counter using usestates

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css'
 import Card from './components/card'
 
@@ -19,14 +20,39 @@ function App() {
       name : "KANE",
       role : "Forward Deployed Engineer"
     }];
+    const [num,setNum]=useState(0);
+    function increase(){
+      setNum(num+1);
+    }
+    function decrease(){
+      if(num == 0) return num
+      setNum(num-1);
+    }
   return (
-
-      <div className="parent">
-        {arr.map(function(elem){
-          return <Card user ={elem.name} pos={elem.role}/>
-        })}
+    <div className="min-h-screen bg-black text-white flex flex-col">
+      
+      <div className="parent p-4 flex-grow">
+        {arr.map((elem, index) => (
+          <Card key={index} user={elem.name} pos={elem.role} />
+        ))}
       </div>
-  )
+      <div className="flex justify-center items-center gap-6 p-6 bg-black">
+        <button 
+          onClick={decrease}
+          className="w-10 h-10 flex items-center justify-center bg-blue-600 text-white rounded-full hover:bg-blue-700 active:scale-95 transition"
+        >
+          -
+        </button>
+         <h4 className="text-3xl font-bold w-12 text-center text-white">{num}</h4>
+        <button 
+          onClick={increase}
+          className="w-10 h-10 flex items-center justify-center bg-blue-600 text-white rounded-full hover:bg-blue-700 active:scale-95 transition"
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App
